27837f4b6c6572a47e57cd25acb81430f10e11cf
Asko Soukka <asko.soukka@iki.fi>
2025-10-26 10:40:52 +0200

    ci: Update

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,8 @@ import tailwindcss from '@tailwindcss/vite';
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [tailwindcss(), sveltekit()],
-	define: {
-		'import.meta.env.PUBLIC_OIDC_AUTHORITY': JSON.stringify(process.env.PUBLIC_OIDC_AUTHORITY),
-		'import.meta.env.PUBLIC_OIDC_CLIENT_ID': JSON.stringify(process.env.PUBLIC_OIDC_CLIENT_ID),
-		'import.meta.env.PUBLIC_OIDC_REDIRECT_URI': JSON.stringify(
-			'https://kortti.suomenpalikkayhteiso.fi/callback'
-		)
-	},
-	base: '/'
+	base: '/',
+	envPrefix: 'VITE_'
 };
 
 export default config;
